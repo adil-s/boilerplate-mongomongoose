172b61bc9f876bd56b6edfcdbc95591b4f294786
adil-s <ah.shaikh97@gmail.com>
2021-11-28 16:13:11 +0530

    adding required constraint on name field of the person schema

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 const personSchema = new Schema({
-  name: { type: String },
+  name: { type: String, required: true},
   age: Number,
   favoriteFood: [String]
 })
